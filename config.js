4f90435ad44c8fde11a0677e814b7684168dbebc
Sad-Husk <58060329+Sad-Husk@users.noreply.github.com>
2024-01-28 23:58:20 -0600

    ok

---
+++ config.js
@@ -0,0 +1,19 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  VITE_CORS_PROXY_URL: "https://superlative-phoenix-132f8f.netlify.app",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYzA3YzRkNWQwNWRmOGI1MjhjYWFhMDg4NmRkNzg5NiIsInN1YiI6IjY1MGQxNWIwMmM2YjdiMDBmZTQ2NjQ3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HoNeFMxTMDpmgNQiM_DoLv7Wo9QyCiB4omIkB8Epwp0",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: false,
+
+  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
+  VITE_BACKEND_URL: null,
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: ""
+};
